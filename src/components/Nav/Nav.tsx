2e1cd9932880ba103f6f605f261e6b51bcae2673
mmorganj41 <mmorganj41@gmail.com>
2023-02-10 15:41:16 -0500

    add css transition to navbar on scroll

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,32 +2,20 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import './Nav.css'
 
-const useWindowScroll = () => {
-    const [windowScroll, setWindowScroll] = useState(0);
-
-    useEffect(() => {
-        const updatePosition = () => {
-            setWindowScroll(window.pageYOffset);
-        }
-
-        window.addEventListener("scroll", updatePosition);
-        updatePosition();
-
-        return () => window.removeEventListener("scroll", updatePosition);
-    }, []);
-
-    return windowScroll;
-}
+const scrollTransition = 64;
 
 
-const StyledNav = styled.nav`
-  height: 50px;
+const StyledNav = styled.nav<{ transition: boolean}>`
+  height: ${({transition}) => transition ? '100px': '50px'};
+  background-color: ${({transition}) => transition ? 'transparent' : 'var(--colorThemeDark)'};
 `
 
-export default function Nav(){
+export default function Nav({useWindowScroll}){
     const scrollY = useWindowScroll();
+    let transition = scrollY > scrollTransition;
+    console.log(transition);
     return (
-      <StyledNav className='navbar Nav'>
+      <StyledNav className='navbar Nav' transition={transition}>
         <span role="img" aria-label="bricks">
           🧱
         </span>{" "}
