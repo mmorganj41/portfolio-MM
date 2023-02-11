e12a53fc4278b7a13c337f309495e2aefd18dd07
mmorganj41 <mmorganj41@gmail.com>
2023-02-11 09:02:42 -0500

    add padding on main section for mobile

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,11 +5,13 @@ import React, {forwardRef} from "react";
 const Main = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <section id='main' className='Main section' ref={ref}>
+            <div className='container'>
             <div>
                 <h1 style={{textTransform: 'uppercase'}} >Hi, I'm <a href='#about'><span className='name'>Michael Morgan</span></a></h1>
                 <p> A Fullstack software engineer with an interest in technology, health, and learning.</p>
             </div>
             <a href='#projects'><button className='btn btn--bg'>Projects</button></a>
+            </div>
         </section>
     )
 });
