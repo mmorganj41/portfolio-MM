5df6c5b0ccbc76bc7f3a4637ab26c9b15a4d133b
mmorganj41 <mmorganj41@gmail.com>
2023-02-11 15:12:49 -0500

    add ration

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,3 +1,5 @@
+import './Project.css';
+
 interface Props {
     image: string;
     name: string;
@@ -7,7 +9,7 @@ interface Props {
 export default function Project(props: Props) {
     const {image, name, summary} = props;
     return (
-        <div>
+        <div className='Project'>
             <img src={image} />
             <h5>{name}</h5>
             <p>{summary}</p>
--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -27,7 +27,7 @@ const battleship = new ProjectInfo('Battleship', 'A browser-based game built in 
 battleship.addBullets(
     'Produced an interactive user interface using HTML, Javascript, and CSS allowing one player to drag and drop ships and select squares to fire upon during battles against an AI opponent.',
     'Made the classic game of Battleship complete with sound effects and animations, and added a feature for tracking wins and losses for repeatable and enjoyable gameplay experience.'
-    );
+);
 battleship.addImages('battleship/PlayingSS.png', '/battleship/draggingSS.png', '/battleship/GameoverSS.png', );
 
 const pokeCatcher = new ProjectInfo('PokeCatcher', 'A MERN-based online game');
@@ -35,15 +35,29 @@ pokeCatcher.addBullets(
     'Constructed a browser game utilizing JavaScript, by interfacing with public APIs and parsing JSON data to mimic traditional Pokemon battle mechanics.',
     'Leveraged MongoDB to store user and pokemon data, integrated with Node.JS, mongoose, and express for inter-player interactions.',
     'Created the full CRUD website application to pokemon training through battles and for tracking individual progression through saved levels, health, and move data using Google OAuth.'
-    );
+);
 pokeCatcher.addImages(
+    '/pokeCatcher/BattleGame.png',
     '/pokeCatcher/Battle.png',
     '/pokeCatcher/All Pokemon.png',
-    '/pokeCatcher/BattleGame.png',
     '/pokeCatcher/NewPokemon.png',
 );
 
-const projects = [pokeCatcher, battleship];
+const ration = new ProjectInfo('Ration', 'A recipe and nutrition tracker built with Django');
+ration.addBullets(
+    'Implemented Git version control and agile development methodologies for software development, specifically a full stack web application using Python and Django.',
+    'Devised functions to make REST API calls to perform CRUD operations on a Postgres database, and implemented image storage on AWS S3.',
+    'Customized forms and data visualization capabilities using JQuery, ChartJS, and JavaScript to display daily meal nutrition information on a recipe website.',
+    'Launched an intuitive and user-friendly recipe website for users to track nutrition and plan four daily meals, providing a solution for fitness enthusiasts.',
+);
+
+ration.addImages(
+    '/ration/Home.png',
+    '/ration/NutritionTracker.png',
+    '/ration/rationList.png',
+)
+
+const projects = [ration, pokeCatcher, battleship];
 
 export default function ProjectList() {
     const projectJSX = projects.map(project => {
