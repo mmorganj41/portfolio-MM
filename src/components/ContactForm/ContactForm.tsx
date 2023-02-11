f0e736f4f821c42bbec3d1f792820091a62807f7
mmorganj41 <mmorganj41@gmail.com>
2023-02-11 10:19:21 -0500

    create handleChange callback function for contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -8,19 +8,28 @@ export default function ContactForm() {
         message: '', 
     })
 
+    function handleChange(e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) {
+        const key = e.target.name;
+        
+        updateForm(draft => {
+            (draft as Record<string, string>)[key] = e.target.value;
+        });
+           
+    }
+
     return (<div className="ContactForm">
         <form>
             <div className="FormField">
                 <label htmlFor='name'>Name</label>
-                <input id="name" type="text" name="name" value={formState.name}/>
+                <input id="name" type="text" name="name" value={formState.name} onChange={handleChange}/>
             </div>
             <div className="FormField">
                 <label htmlFor='email'>Email</label>
-                <input id="email" type="email" name="email" value={formState.email}/>
+                <input id="email" type="email" name="email" value={formState.email} onChange={handleChange}/>
             </div>
             <div className="FormField">
                 <label htmlFor='message'>Message</label>
-                <textarea id="message" cols={30} rows={10} name="message" value={formState.message}/>
+                <textarea id="message" cols={30} rows={10} name="message" value={formState.message} onChange={handleChange}/>
             </div>
             <button className="btn btn--bg" type="submit">Submit</button>
         </form>
