e839147bfd4380b73b42337dbd234af3249b7d92
mmorganj41 <mmorganj41@gmail.com>
2023-02-11 08:26:29 -0500

    make responsive sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import './Sidebar.css';
 
 import MenuList from '../MenuList/MenuList';
+import styled from 'styled-components';
 
 interface Props {
     open: boolean;
@@ -8,11 +9,16 @@ interface Props {
     navLocation: number;
 }
 
+const StyledHamburgerMenu = styled.ul<{ open: boolean}>`
+    transform: ${({open}) => open ? 'translateX(0px)' : 'translateX(250px)'}
+`
+
 export default function Sidebar(props: Props) {
     const {open, transition, navLocation} = props;
+
     return (
-        <div className='Sidebar'>
+        <StyledHamburgerMenu className='Sidebar' open={open}>
             <MenuList navLocation={navLocation} inline={false} />
-        </div>
+        </StyledHamburgerMenu>
     )
 }
