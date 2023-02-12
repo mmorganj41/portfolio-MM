762810ac2b492b71730d550206d5e8215d31048f
mmorganj41 <mmorganj41@gmail.com>
2023-02-12 07:56:53 -0500

    fix overlap on mobile in about and add image

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -5,9 +5,13 @@ export default function Info() {
     return (
         <div className='Info'>
             <h3 className='title'>About Me</h3>
+            
             <div className='details'>
                 <p>A <strong>software engineer</strong> with a 5+ year background in biomedical engineering and consulting with demonstrated competence in finding solutions to complex problems.</p>
                 <p>Understands and can communicate technical information to a variety of individuals, developed through experiences in diverse collaborations. Continuously looks to improve, as shown by continuous re-iteration of technology and optimization of projects on a team and in personal life.</p>
+            </div>
+            <div className='photo'>
+                <img src='https://i.imgur.com/I2jrKKp.jpg' />
             </div>
             <div className='socials'>
                 <SocialIcon icon='linkedin' link='https://www.linkedin.com/in/michael-morgan-j41/' samePage={false}/>
