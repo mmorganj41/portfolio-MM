2ad95afa524a3c6c89fba8f3d332bc22c892b682
mmorganj41 <mmorganj41@gmail.com>
2023-03-07 15:17:00 -0500

    add handleClick to increase index

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -19,11 +19,19 @@ export default function ProjectList(props: Props) {
         return <Project key={project.name} liftIndex={liftIndex} selectedIndex={selectedIndex} index={i} image={project.images[0]} name={project.name} summary={project.summary} setCarouselIndex={setCarouselIndex}/>
     }) 
 
+    function handleClick() {
+        setLoadedIndex(index => index + 4);
+    }
+
     return (
         <div className='ProjectList'>
             <h2 className='title'>Project List</h2>
-            {projectJSX.slice(0, loadedIndex)}
-            {loadedIndex < projectJSX.length - 1 && <button className='load btn'>Load more</button>}
+            {projectJSX.slice(0, loadedIndex+1)}
+            {loadedIndex < projectJSX.length - 1 && (
+            <button onClick={handleClick} className='load btn'>
+                Load more
+            </button>
+            )}
         </div>
     )
 }
